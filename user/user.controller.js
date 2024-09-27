fe225bd824df0469256806ae775a5ba15307e975
ssmarika <smarikashrestha02@gmail.com>
2024-09-27 17:07:19 +0545

    Day4.3 LoginUser completed

--- user/user.controller.js
+++ user/user.controller.js
@@ -7,13 +7,16 @@ import {
   userValidationSchema,
 } from "./user.validation.js";
 import validateReqBody from "../middleware/authentication.middleware.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 // !register user
 router.post(
   "/user/register",
+  //validate the request body using middleware
   validateReqBody(userValidationSchema),
+
   async (req, res) => {
     //extract new user from req.body
     const newUser = req.body;
@@ -45,9 +48,42 @@ router.post(
 // !LOGIN
 router.post(
   "/user/login",
+  //validate req body using the middleware
   validateReqBody(loginUserValidationSchema),
-  (req, res) => {
-    return res.status(200).send("Login...");
+
+  async (req, res) => {
+    //extract the login credentials from req.body
+    const data = req.body;
+
+    // find user using email
+    const user = await User.findOne({ email: data.email });
+
+    // if not throw error
+    if (!user) {
+      return res.status(400).send({ message: "Invalid Credentials" });
+    }
+
+    // compare password using decrypt
+    const plainPassword = data.password;
+    const hashedPassword = user.password;
+    const isPasswordMarch = await bcrypt.compare(plainPassword, hashedPassword);
+
+    // if not password ,match, throw error
+    if (!isPasswordMarch) {
+      return res.status(404).send({ message: "Invalid Credentials" });
+    }
+    //generate access token
+    const payload = { email: user.email };
+    const secretKey = "SECRETKEY";
+    const token = jwt.sign(payload, secretKey);
+
+    return res
+      .status(200)
+      .send({
+        message: "Successful login",
+        userDetail: user,
+        accessToken: token,
+      });
   }
 );
 
